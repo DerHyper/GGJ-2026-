6ccf887100501d896dbf4b19946376d983d126cb
Max <max@ahoiii.com>
2026-02-01 15:44:18 +0100

    Add Start Menu Sound

--- Illumibirds/Assets/_Scripts/Managers/MainMenu.cs
+++ Illumibirds/Assets/_Scripts/Managers/MainMenu.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class MainMenu : MonoBehaviour
 {
     public MusicTrack music;
+    public AudioClip startGameSound;
+    public float startGameSoundVolume = 0.3f;
     private void Start() 
     {
         AudioManager.Instance.PlayMusic(music);
@@ -10,6 +12,7 @@ public class MainMenu : MonoBehaviour
    public void StartGame()
     {
         GameManager.Instance.StartGame();
+        AudioManager.Instance.PlayOnce(startGameSound, startGameSoundVolume);
     }
 
     public static void QuitGame()
