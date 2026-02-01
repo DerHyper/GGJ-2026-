508be032e73c615debd46b934eac2880b6f3379b
Max <max@ahoiii.com>
2026-02-01 15:00:41 +0100

    Facelift Main Menu

--- Illumibirds/Assets/_Scripts/Managers/MainMenu.cs
+++ Illumibirds/Assets/_Scripts/Managers/MainMenu.cs
@@ -2,6 +2,11 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
+    public MusicTrack music;
+    private void Start() 
+    {
+        AudioManager.Instance.PlayMusic(music);
+    }
    public void StartGame()
     {
         GameManager.Instance.StartGame();
